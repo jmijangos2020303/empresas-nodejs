d73fb1e6e1ec785b75dcf91d5b7f3f636d5bee25
José Flavio Xavier Mijangos Vasquez <jmijangos-2020303@kinal.edu.gt>
2022-04-07 10:10:39 -0600

    7/04/2022

--- src/routes/empresas.routes.js
+++ src/routes/empresas.routes.js
@@ -7,6 +7,6 @@ const api = express.Router();
 
 api.post('/registrarEmpresa', md_autenticacion.Auth ,controladorEmpresa.RegistrarEmpresa);
 api.get('/verEmpresas',md_autenticacion.Auth ,controladorEmpresa.visualizarEmpresas);
-api.delete('/eliminarEmpresa/:idCat', controladorEmpresa.EliminarEmpresa);
+api.delete('/eliminarEmpresa/:idCat', md_autenticacion.Auth, controladorEmpresa.EliminarEmpresa);
 
 module.exports = api;
